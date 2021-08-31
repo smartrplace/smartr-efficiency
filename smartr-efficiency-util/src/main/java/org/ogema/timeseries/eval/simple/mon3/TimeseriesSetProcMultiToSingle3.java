5e4dc76d024899cd2a24ba37863c05f7b098dff7
David Nestle <david.nestle@iee.fraunhofer.de>
2021-08-31 20:16:02 +0200

    eval efficiency update

--- smartr-efficiency-util/src/main/java/org/ogema/timeseries/eval/simple/mon3/TimeseriesSetProcMultiToSingle3.java
+++ smartr-efficiency-util/src/main/java/org/ogema/timeseries/eval/simple/mon3/TimeseriesSetProcMultiToSingle3.java
@@ -303,6 +303,10 @@ if(tsSingleLog != null) tsSingleLog.logEvent((endOfCalc-startOfCalc), "Calculati
 		long start;
 		if(ts.getLastEndTime() <= 0) {
 			ts.loadInitData();
+if(Boolean.getBoolean("evaldebug0")) {
+	int newTs = ts.size();
+	System.out.println("Reading "+newTs+" timestamps into "+ts.dpLabel());
+}
 			if(ts.getLastEndTime() <= 0) {
 				Long startRaw = ts.getFirstTimeStampInSource();
 				if(startRaw == null)
