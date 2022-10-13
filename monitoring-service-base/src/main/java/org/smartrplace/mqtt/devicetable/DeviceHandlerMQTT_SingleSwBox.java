c1fafefd6f9971733fc9d2415b4d590b7a039da8
David Nestle <david.nestle@iee.fraunhofer.de>
2022-10-13 14:09:07 +0200

    fix SingleSwitchBox device handler

--- monitoring-service-base/src/main/java/org/smartrplace/mqtt/devicetable/DeviceHandlerMQTT_SingleSwBox.java
+++ monitoring-service-base/src/main/java/org/smartrplace/mqtt/devicetable/DeviceHandlerMQTT_SingleSwBox.java
@@ -6,23 +6,22 @@ import java.util.List;
 
 import org.ogema.accessadmin.api.ApplicationManagerPlus;
 import org.ogema.core.application.ApplicationManager;
-import org.ogema.core.model.Resource;
 import org.ogema.core.model.simple.BooleanResource;
 import org.ogema.core.model.simple.FloatResource;
 import org.ogema.core.model.simple.IntegerResource;
 import org.ogema.core.model.simple.SingleValueResource;
+import org.ogema.core.model.units.PowerResource;
+import org.ogema.core.model.units.TemperatureResource;
 import org.ogema.core.resourcemanager.ResourceValueListener;
 import org.ogema.core.resourcemanager.pattern.ResourcePattern;
 import org.ogema.core.resourcemanager.pattern.ResourcePatternAccess;
 import org.ogema.devicefinder.api.Datapoint;
 import org.ogema.devicefinder.api.DatapointService;
-import org.ogema.devicefinder.api.InstalledAppsSelector;
 import org.ogema.devicefinder.util.DeviceHandlerSimple;
-import org.ogema.devicefinder.util.DeviceTableBase;
 import org.ogema.eval.timeseries.simple.smarteff.AlarmingUtiH;
 import org.ogema.model.communication.CommunicationStatus;
 import org.ogema.model.devices.sensoractordevices.SingleSwitchBox;
-import org.ogema.model.locations.Room;
+import org.ogema.model.sensors.TemperatureSensor;
 import org.ogema.simulation.shared.api.RoomInsideSimulationBase;
 import org.ogema.simulation.shared.api.SingleRoomSimulationBase;
 import org.ogema.tools.resourcemanipulator.timer.CountDownDelayedExecutionTimer;
@@ -30,7 +29,6 @@ import org.smartrplace.apps.hw.install.config.HardwareInstallConfig;
 import org.smartrplace.apps.hw.install.config.InstallAppDevice;
 import org.smartrplace.util.directobjectgui.ObjectResourceGUIHelper;
 
-import de.iwes.widgets.api.widgets.WidgetPage;
 import de.iwes.widgets.api.widgets.sessionmanagement.OgemaHttpRequest;
 import de.iwes.widgets.html.alert.Alert;
 import de.iwes.widgets.html.complextable.RowTemplate.Row;
@@ -52,7 +50,7 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 		return SingleSwitchBox.class;
 	}
 
-	@Override
+	/*@Override
 	public DeviceTableBase getDeviceTable(WidgetPage<?> page, Alert alert,
 			InstalledAppsSelector appSelector) {
 		return new DeviceTableBase(page, appMan, alert, appSelector, this) {
@@ -102,6 +100,24 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 				return DeviceHandlerMQTT_SingleSwBox.this.id();
 			}
 		};
+	}*/
+	
+	@Override
+	protected void addMoreValueWidgets(InstallAppDevice object, SingleSwitchBox device,
+			ObjectResourceGUIHelper<InstallAppDevice, InstallAppDevice> vh, String id, OgemaHttpRequest req, Row row,
+			ApplicationManager appMan, Alert alert) {
+		PowerResource power = device.electricityConnection().powerSensor().reading();
+		TemperatureResource temperature = device.getSubResource("temperatureSensor", TemperatureSensor.class).reading();
+		if((req != null) && (!power.isActive()) && (!temperature.isActive()))
+			return;
+		Label stateFB = vh.booleanLabel("StateFB", id, device.onOffSwitch().stateFeedback(), row, 0);
+		Label lastFB = addLastContact("Last FB", vh, "LFB"+id, req, row, device.onOffSwitch().stateFeedback());
+		if(stateFB != null)
+			stateFB.setDefaultPollingInterval(DEFAULT_POLL_RATE);
+		if(lastFB != null)
+			lastFB.setDefaultPollingInterval(DEFAULT_POLL_RATE);
+
+		vh.booleanEdit("Control", id, device.onOffSwitch().stateControl(), row);
 	}
 
 	@Override
@@ -160,6 +176,8 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 	
 	@Override
 	public Collection<Datapoint> getDatapoints(SingleSwitchBox dev, InstallAppDevice appDevice) {
+		//FIXME Workaround
+		dev.activate(true);
 		//SingleSwitchBox dev = (SingleSwitchBox) appDevice.device();
 		List<Datapoint> result = new ArrayList<>();
 		addDatapoint(dev.onOffSwitch().stateControl(), result, dpService);
@@ -183,7 +201,8 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 				restartReason.create().activate(false);
 			addDatapoint(restartReason, result, dpService, true);
 		}
-			
+		addDatapoint(dev.getSubResource("temperatureSensor", TemperatureSensor.class).reading(), result, dpService);			
+		
 		addtStatusDatapointsHomematic(dev, dpService, result, false);
 		
 		return result;
@@ -193,10 +212,15 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 	public void initAlarmingForDevice(InstallAppDevice appDevice, HardwareInstallConfig appConfigData) {
 		appDevice.alarms().create();
 		SingleSwitchBox device = (SingleSwitchBox) appDevice.device();
+		boolean isShelly = device.getLocation().startsWith("shellies");
 		AlarmingUtiH.setTemplateValues(appDevice, device.onOffSwitch().stateFeedback(),
-			0.0f, 1.0f, 1, AlarmingUtiH.DEFAULT_NOVALUE_MINUTES);
+			0.0f, 1.0f, 1, isShelly?-1:AlarmingUtiH.DEFAULT_NOVALUE_MINUTES);
 		AlarmingUtiH.setTemplateValues(appDevice, device.electricityConnection().powerSensor().reading(),
-				0.0f, 4000.0f, 10, -1);
+				0.0f, 4000.0f, 10, isShelly?AlarmingUtiH.DEFAULT_NOVALUE_MINUTES:-1);
+		if(isShelly) {
+			AlarmingUtiH.setTemplateValues(appDevice, device.getSubResource("temperatureSensor", TemperatureSensor.class).reading(),
+					0f, 55f, 10, isShelly?AlarmingUtiH.DEFAULT_NOVALUE_MINUTES:-1);			
+		}
 		//AlarmingUtiH.setTemplateValues(appDevice, device.electricityConnection().voltageSensor().reading(),
 		//		180f, 350f, 10, 20);
 		//For tasmota voltage zero is sent when switch is off
@@ -208,7 +232,7 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 	
 	@Override
 	public String getInitVersion() {
-		return "D";
+		return "E";
 	}
 
 	@Override
@@ -223,6 +247,12 @@ public class DeviceHandlerMQTT_SingleSwBox extends DeviceHandlerSimple<SingleSwi
 
 	@Override
 	public SingleValueResource getMainSensorValue(SingleSwitchBox box, InstallAppDevice deviceConfiguration) {
+		PowerResource power = box.electricityConnection().powerSensor().reading();
+		if(power.isActive())
+			return power;
+		TemperatureResource temperature = box.getSubResource("temperatureSensor", TemperatureSensor.class).reading();
+		if(temperature.isActive())
+			return temperature;
 		return box.onOffSwitch().stateFeedback();
 	}
 }
