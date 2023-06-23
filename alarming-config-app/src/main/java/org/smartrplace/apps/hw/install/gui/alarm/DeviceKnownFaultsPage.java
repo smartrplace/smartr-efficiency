24ed68fa48d4c614219190ecc4d205c286669c92
cnoelle <christoph.noelle@smartrplace.de>
2023-06-23 11:42:08 +0200

    alarming config gui: last message popup extended

--- alarming-config-app/src/main/java/org/smartrplace/apps/hw/install/gui/alarm/DeviceKnownFaultsPage.java
+++ alarming-config-app/src/main/java/org/smartrplace/apps/hw/install/gui/alarm/DeviceKnownFaultsPage.java
@@ -103,6 +103,11 @@ public class DeviceKnownFaultsPage extends DeviceAlarmingPage {
 	private final Label lastMessageRoom;
 	private final Label lastMessageLocation;
 	private final Label lastMessage;
+	private final Label lastMessageComment;
+	private final Label lastMessageTaskTracking;
+	private final Label lastMessageAssigned;
+	private final Label lastMessageResponsible;
+	
 	private final ReleasePopup releasePopup;
 	
 	public static final Map<String, String> dignosisVals = new HashMap<>();
@@ -146,19 +151,27 @@ public class DeviceKnownFaultsPage extends DeviceAlarmingPage {
 		this.lastMessageRoom = new Label(page, "lastMessagePopupRoom");
 		this.lastMessageLocation = new Label(page, "lastMessagePopupLocation");
 		this.lastMessage = new Label(page, "lastMessage");
+		this.lastMessageComment = new Label(page, "lastMessagePopupComment");
+		this.lastMessageTaskTracking = new Label(page, "lastMessagePopupTT");
+		this.lastMessageAssigned = new Label(page, "lastMessagePopupAssigned");
+		this.lastMessageResponsible = new Label(page, "lastMessagePopupResponsible");
 		
-		
-		final StaticTable tab = new StaticTable(4, 2, new int[]{3, 9});
+		final StaticTable tab = new StaticTable(8, 2, new int[]{3, 9});
 		tab.setContent(0, 0, "Device").setContent(0, 1, lastMessageDevice)
 			.setContent(1, 0, "Room").setContent(1, 1, lastMessageRoom)
 			.setContent(2, 0, "Location").setContent(2, 1, lastMessageLocation)
-			.setContent(3, 0, "Message").setContent(3,1, lastMessage);
+			.setContent(3, 0, "Message").setContent(3,1, lastMessage)
+			.setContent(4, 0, "Comment").setContent(4, 1, lastMessageComment)
+			.setContent(5, 0, "Assigned").setContent(5, 1, lastMessageAssigned)
+			.setContent(6, 0, "Responsible").setContent(6, 1, lastMessageResponsible)
+			.setContent(7, 0, "Task tracking").setContent(7, 1, lastMessageTaskTracking);
 		final PageSnippet snip = new PageSnippet(page, "lastMessageSnip", true);
 		snip.append(tab, null);
 		lastMessagePopup.setBody(snip, null);
 		final Button closeLastMessage = new Button(page, "lastMessageClose", "Close");
 		closeLastMessage.triggerAction(lastMessagePopup, TriggeringAction.ON_CLICK, TriggeredAction.HIDE_WIDGET);
 		lastMessagePopup.setFooter(closeLastMessage, null);
+		lastMessagePopup.setMinWidth("35em", null);
 		page.append(lastMessagePopup);
 		//
 		finishConstructor();
@@ -537,14 +550,29 @@ public class DeviceKnownFaultsPage extends DeviceAlarmingPage {
 				vh.timeLabel("Started", id, res.ongoingAlarmStartTime(), row, 0);
 				final Button showMsg = new Button(mainTable, "msg" + id, req) {
 					
+					private String getOrEmpty(final StringResource res) {
+						return res.isActive() ? res.getValue() : "--";
+					}
+					
 					@Override
 					public void onPOSTComplete(String data, OgemaHttpRequest req) {
 						lastMessage.setText(res.lastMessage().getValue(), req);
 						lastMessageDevice.setText(object.deviceId().getValue(), req);
 						final String room = device.location().room().isActive() ? ResourceUtils.getHumanReadableShortName(device.location().room()) : "--";
 						lastMessageRoom.setText(room, req);
-						final String location = object.installationLocation().isActive() ? object.installationLocation().getValue() : "--";
-						lastMessageLocation.setText(location, req);
+						lastMessageLocation.setText(getOrEmpty(object.installationLocation()), req);
+						lastMessageComment.setText(getOrEmpty(res.comment()), req);
+						String role = "--";
+						if (res.assigned().isActive()) {
+							final int assigned = res.assigned().getValue();
+							final String roleAssigned = AlarmingConfigUtil.ASSIGNEMENT_ROLES.get(assigned);
+							if (roleAssigned != null)
+								role = roleAssigned;
+						}
+						lastMessageAssigned.setText(role, req);
+						lastMessageTaskTracking.setText(getOrEmpty(res.linkToTaskTracking()), req);
+						lastMessageResponsible.setText(getOrEmpty(res.responsibility()), req);
+						
 					}
 					
 				};
@@ -554,6 +582,10 @@ public class DeviceKnownFaultsPage extends DeviceAlarmingPage {
 				showMsg.triggerAction(lastMessageDevice,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
 				showMsg.triggerAction(lastMessageRoom,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
 				showMsg.triggerAction(lastMessageLocation,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
+				showMsg.triggerAction(lastMessageComment,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
+				showMsg.triggerAction(lastMessageAssigned,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
+				showMsg.triggerAction(lastMessageTaskTracking,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
+				showMsg.triggerAction(lastMessageResponsible,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
 				
 				showMsg.triggerAction(lastMessage,  TriggeringAction.POST_REQUEST, TriggeredAction.GET_REQUEST, req);
 				row.addCell("Message", showMsg);
